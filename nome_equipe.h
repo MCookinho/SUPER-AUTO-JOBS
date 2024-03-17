972f69106211cfb6a07ae2f3a06466a49c05612d
João Pedro Vieira Góes <144862146+jpvgoes@users.noreply.github.com>
2024-03-17 20:18:45 -0300

    Create nome_equipe.h
    biblioteca dos nomes das equipes

---
+++ nome_equipe.h
@@ -0,0 +1,14 @@
+#ifndef NOME_EQUIPE
+#define NOME_EQUIPE
+#include <stdio.h>
+
+//criação do tipo dos nomes das equipes.
+typedef struct 
+{
+    char adjetivo[20];
+    char substantivo[20];
+}tp_nome;
+
+
+
+#endif
