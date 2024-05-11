51eb3dad495fd0fbdf81b1091ad0e416c92ebb6f
MCookinho <148594362+MisterCookinho@users.noreply.github.com>
2024-05-11 20:48:53 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,12 +7,19 @@ int main(){
 	//Criação das variaveis dos nomes
 	tp_nome_equipe nomeeqp, cpunome;
 	
+	//Variavel do dinheiro da loja
+	int dinhero;
+	
 	//definição da mão
 	tp_animal m1, m2, m3;
 	
 	//funções de apresentação pra entrega da AV1
+	if(!telaInicio()){
+		return 0;
+	}
+	
 	faznome(&nomeeqp, &cpunome);
-	faz_mao(&m1, &m2, &m3);
+	faz_loja(&m1, &m2, &m3);
 	printf("\n\n");
 	printanome(nomeeqp);
 	printf("\n");
