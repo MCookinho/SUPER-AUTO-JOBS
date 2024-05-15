bc993258d8b986bcfc2811fe39b137a75798fad4
MCookinho <148594362+MisterCookinho@users.noreply.github.com>
2024-05-14 22:29:29 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
 #include "animain.h"
 #include "randname.h"
 #include "animao.h"
-#include <string.h>
+#include "anibatalha.h"
 
 int main(){
 	//Criação das variaveis dos nomes
 	tp_nome_equipe nomeeqp, cpunome;
 	//definição da mão
-	tp_compra preloja[3];
-	tp_animal mao[5];
+	tp_compra preloja[3], mao[5];
 	limpa_mao(mao);
 	//definição da fase e vida
-	int fase, vida;
-	//funções de apresentação pra entrega da AV1
+	int vidaplayer=5, vidacpu=5;
+	//Menu
 	if(!telaInicio()){
 		return 0;
 	}
-	
+	//funcoes
 	faznome(&nomeeqp, &cpunome);
 	faz_loja(preloja);
-	escolhe_loja(preloja, mao);
-	printanome(nomeeqp);
-	printf("\n");
-	printanome(cpunome);
-	printf("\n");
+	escolhe_loja(preloja, mao, vidaplayer);
+	batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu);
+	escolhe_loja(preloja, mao, vidaplayer);
 }
