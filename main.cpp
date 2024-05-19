f58a779d7670aa9945a853f5f306a1f16c840172
MCookinho <148594362+MisterCookinho@users.noreply.github.com>
2024-05-19 01:32:46 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,21 +4,20 @@
 #include "anibatalha.h"
 
 int main(){
-	//Criação das variaveis dos nomes
+	//Criacao das variaveis dos nomes
 	tp_nome_equipe nomeeqp, cpunome;
-	//definição da mão
-	tp_compra preloja[3], mao[5];
+	//definiÃ§Ã£o da mÃ£o
+	tp_compra mao[5];
 	limpa_mao(mao);
-	//definição da fase e vida
+	//definicao da fase e vida
 	int vidaplayer=5, vidacpu=5;
 	//Menu
 	if(!telaInicio()){
 		return 0;
 	}
 	//funcoes
+	escolhe_loja(mao, vidaplayer);
 	faznome(&nomeeqp, &cpunome);
-	faz_loja(preloja);
-	escolhe_loja(preloja, mao, vidaplayer);
 	batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu);
-	escolhe_loja(preloja, mao, vidaplayer);
+	escolhe_loja(mao, vidaplayer);
 }
