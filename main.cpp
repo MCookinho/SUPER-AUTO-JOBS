73b57053be7214f2956526d3eaa0c3ac41611c84
MCookinho <148594362+MisterCookinho@users.noreply.github.com>
2024-05-24 16:45:59 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,20 @@
 int main(){
 	//Criacao das variaveis dos nomes
 	tp_nome_equipe nomeeqp, cpunome;
+	int config[3];
+	restaura_config(config);
 	//definicao da mao
 	tp_compra mao[5];
 	limpa_mao(mao);
 	//definicao da fase e vida
 	int vidaplayer=5, vidacpu=5;
 	//Menu
-	if(!telaInicio()){
+	if(!telaInicio(config)){
 		return 0;
 	}
 	//funcoes
 	escolhe_loja(mao, vidaplayer);
 	faznome(&nomeeqp, &cpunome);
-	batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu);
+	batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu, config);
 	escolhe_loja(mao, vidaplayer);
 }
