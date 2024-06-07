9a3cd8c2e8f3b0a38fc213fb4ed6aa9fd64e5f8e
Peu Borges <148594362+MCookinho@users.noreply.github.com>
2024-06-07 19:39:51 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,16 +10,29 @@ int main(){
 	restaura_config(config);
 	//definicao da mao
 	tp_compra mao[5];
-	limpa_mao(mao);
-	//definicao da fase e vida
-	int vidaplayer=5, vidacpu=5;
-	//Menu
-	if(!telaInicio(config)){
-		return 0;
-	}
-	//funcoes
-	escolhe_loja(mao, vidaplayer);
-	faznome(&nomeeqp, &cpunome);
-	batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu, config);
-	escolhe_loja(mao, vidaplayer);
+	while(1==1){
+		limpa_mao(mao);
+		//definicao da fase e vida
+		int vidaplayer=5, vidacpu=5, fase=1;
+		//Menu
+		if(!telaInicio(config)){
+			return 0;
+		}
+		//Jogo
+		escolhe_loja(mao, vidaplayer);//primeiro turno
+		faznome(&nomeeqp, &cpunome);
+		batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu, config, fase);
+		
+		while(vidaplayer > 0 && vidacpu > 0){//outros turnos
+		fase+=1;
+		escolhe_loja(mao, vidaplayer);
+		batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu, config, fase);
+		}
+		
+		if(vidaplayer <= 0)
+			fim_de_jogo(0);
+			
+		if(vidacpu <= 0)
+			fim_de_jogo(1);
+	}	
 }
