c0e7b2d293f99cf2ccdf7c2d0731b4a4350ea77c
Peu Borges <148594362+MCookinho@users.noreply.github.com>
2024-06-20 03:03:42 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,25 +2,28 @@
 #include "randname.h"
 #include "animao.h"
 #include "anibatalha.h"
-
+#include "anifases.h"
 
 int singleplayer(tp_nome_equipe nomeeqp, tp_nome_equipe cpunome, int config[3], tp_compra mao[5], int vidaplayer, int vidacpu, int fase){
 	
 	escolhe_loja(mao, vidaplayer);//primeiro turno
 	faznome(&nomeeqp, &cpunome);
+	grava_historico_SP(mao, 0, nomeeqp, cpunome);
 	SP_batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu, config, fase);
 	
 	while(vidaplayer > 0 && vidacpu > 0){//outros turnos
 	fase+=1;
 	escolhe_loja(mao, vidaplayer);
+	grava_historico_SP(mao, 1, nomeeqp, cpunome);
 	SP_batalha(mao, nomeeqp, cpunome, &vidaplayer, &vidacpu, config, fase);
 	}
 	
-	if(vidaplayer <= 0)
-		fim_de_jogo(0);
-		
-	if(vidacpu <= 0)
-		fim_de_jogo(1);
+	if(vidaplayer <= 0){
+		grava_historico_SP(mao, 2, nomeeqp, cpunome);
+		fim_de_jogo(0);}
+	if(vidacpu <= 0){
+		grava_historico_SP(mao, 3, nomeeqp, cpunome);
+		fim_de_jogo(1);}
 }
 
 int multiplayer(tp_nome_equipe nome_P1, tp_nome_equipe nome_P2, int config[3], tp_compra mao_P1[5], int vida_P1, int vida_P2){
@@ -35,6 +38,7 @@ int multiplayer(tp_nome_equipe nome_P1, tp_nome_equipe nome_P2, int config[3], t
 	printa_jogador_mp(2);
 	escolhe_loja(mao_P2, vida_P2);
 	faznome(&nome_P2, &nome_vazio);
+	grava_historico_MP(mao_P1, mao_P2, 0, nome_P1, nome_P2);
 	MP_batalha(mao_P1, mao_P2, nome_P1, nome_P2, &vida_P1, &vida_P2, config);
 	
 	while(vida_P1 > 0 && vida_P2 > 0){//outros turnos
@@ -42,14 +46,17 @@ int multiplayer(tp_nome_equipe nome_P1, tp_nome_equipe nome_P2, int config[3], t
 		escolhe_loja(mao_P1, vida_P1);
 		printa_jogador_mp(2);
 		escolhe_loja(mao_P2, vida_P2);
+		grava_historico_MP(mao_P1, mao_P2, 1, nome_P1, nome_P2);
 		MP_batalha(mao_P1, mao_P2, nome_P1, nome_P2, &vida_P1, &vida_P2, config);
 		}
 		
-		if(vida_P1 <= 0)
-			fim_de_jogo(2);
+		if(vida_P1 <= 0){
+			grava_historico_MP(mao_P1, mao_P2, 2, nome_P1, nome_P2);
+			fim_de_jogo(2);}
 			
-		if(vida_P2 <= 0)
-			fim_de_jogo(3);
+		if(vida_P2 <= 0){
+			grava_historico_MP(mao_P1, mao_P2, 3, nome_P1, nome_P2);
+			fim_de_jogo(3);}
 			
 }
 	
